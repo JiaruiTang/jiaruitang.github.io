ce5bea9d5e1af96cb565686d14509e148148e7ce
Jiarui Tang <jiarui_tang@outlook.com>
2024-03-09 02:29:02 -0500

    Upload new files

---
+++ assets/js/main.js
@@ -0,0 +1,277 @@
+/*
+	Strata by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var $window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$footer = $('#footer'),
+		$main = $('#main'),
+		settings = {
+
+			// Parallax background effect?
+				parallax: true,
+
+			// Parallax factor (lower = more intense, higher = less intense).
+				parallaxFactor: 20
+
+		};
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1800px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ '481px',   '736px'  ],
+			xsmall:  [ null,      '480px'  ],
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Touch?
+		if (browser.mobile) {
+
+			// Turn on touch mode.
+				$body.addClass('is-touch');
+
+			// Height fix (mostly for iOS).
+				window.setTimeout(function() {
+					$window.scrollTop($window.scrollTop() + 1);
+				}, 0);
+
+		}
+
+	// Footer.
+		breakpoints.on('<=medium', function() {
+			$footer.insertAfter($main);
+		});
+
+		breakpoints.on('>medium', function() {
+			$footer.appendTo($header);
+		});
+
+	// Header.
+
+		// Parallax background.
+
+			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+				if (browser.name == 'ie'
+				||	browser.mobile)
+					settings.parallax = false;
+
+			if (settings.parallax) {
+
+				breakpoints.on('<=medium', function() {
+
+					$window.off('scroll.strata_parallax');
+					$header.css('background-position', '');
+
+				});
+
+				breakpoints.on('>medium', function() {
+
+					$header.css('background-position', 'left 0px');
+
+					$window.on('scroll.strata_parallax', function() {
+						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+					});
+
+				});
+
+				$window.on('load', function() {
+					$window.triggerHandler('scroll');
+				});
+
+			}
+
+	// Main Sections: Two.
+
+		// Lightbox gallery.
+			$window.on('load', function() {
+
+				$('#two').poptrox({
+					caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item a.image',
+					usePopupCaption: true,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+				});
+
+			});
+	
+
+})(jQuery);
+
+// Humburger Menu.
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+
+// Define variables useful in Navigation Focus part
+const sections = document.querySelectorAll("#main > section");
+const navLi = document.querySelectorAll(".nav-links a");
+
+// Navigation Bar on Scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.documentElement.scrollTop > 120 & !document.getElementById("scroll-nav").classList.contains("hide")) {
+		document.getElementById("scroll-nav").style.display = "flex";
+		document.getElementById("scroll-nav").style.top = "0px";
+		if (document.getElementById("openbtn").classList.contains("show")) {
+			document.getElementById("openbtn").classList.remove('show');
+		}
+		//document.getElementById("openbtn").className = "openbtn";
+	} else {
+		var obj = document.getElementById("scroll-nav");
+		obj.style.display="none";
+		if (document.documentElement.scrollTop > 150) {
+			document.getElementById("openbtn").classList.add('show');
+		}
+		else {
+			if (document.getElementById("openbtn").classList.contains("show")) {
+				document.getElementById("openbtn").classList.remove('show');
+			}
+			//document.getElementById("openbtn").className = "openbtn";
+		}
+	}
+
+	
+	// Navigation Focus
+	var current = "";
+	sections.forEach((section) => {
+		const sectionTop = section.offsetTop;
+		if (document.documentElement.scrollTop >= sectionTop - 180) {
+			current = section.getAttribute("id"); 
+		}
+	});
+	
+	navLi.forEach((li) => {
+		li.classList.remove("focus");
+		if (li.hash=="#"+current) {
+			li.classList.add("focus");
+		}
+		if (current=="") {
+			document.getElementById("about-link-top").classList.add("focus");
+		}
+	});	
+}
+
+// Prevent fixed navigations covering content
+const navigationHeight = document.querySelector('#scroll-nav').offsetHeight;
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight - 20 + "px");
+
+
+// Close when someone clicks on the "x" symbol inside the overlay
+function closeNav() {
+	document.getElementById("scroll-nav").style.display = "none";
+	document.getElementById("scroll-nav").classList.add('hide');
+	document.getElementById("openbtn").classList.add('show');
+}
+
+// Show the scroll navigation bar when the button was clicked
+function openNav() {
+	document.getElementById("scroll-nav").style.display = "flex";
+	//document.getElementById("scroll-nav").className = "scroll-nav";
+	//document.getElementById("openbtn").className = "openbtn";
+	if (document.getElementById("scroll-nav").classList.contains("hide")) {
+		document.getElementById("scroll-nav").classList.remove('hide');
+	}
+	if (document.getElementById("openbtn").classList.contains("show")) {
+		document.getElementById("openbtn").classList.remove('show');
+	}
+}
+
+
+// Fades in the button when you scroll down
+var link = document.getElementById("back-to-top");
+var amountScrolled = 250;
+  
+window.addEventListener('scroll', function(e) {
+	if ( document.documentElement.scrollTop > amountScrolled ) {
+		link.classList.add('show');
+	} else {
+		link.className = 'back-to-top';
+	}
+});
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+// Skill section bar
+document.addEventListener('DOMContentLoaded', (event) => {
+	const skillPercentages = document.querySelectorAll('.skill-percentage');
+	skillPercentages.forEach(skillPercentage => {
+		const width = skillPercentage.style.width;
+		skillPercentage.style.width = '0';
+		setTimeout(() => {
+		skillPercentage.style.width = width;
+		}, 500);
+	});
+
+
+	// Attach event listeners to the pie chart segments
+	document.querySelector('#programming-container-pie').addEventListener('mouseover', function() {
+		showContainer('programming-container');
+	});
+	document.querySelector('#skills-wrapper-pie').addEventListener('mouseover', function() {
+		showContainer('skills-wrapper');
+	});
+	document.querySelector('#soft-list-pie').addEventListener('mouseover', function() {
+		showContainer('soft-list');
+	});
+
+	// Adding the mouseout event listeners
+	document.querySelector('#programming-container-pie').addEventListener('mouseout', showSkillsIntro);
+	document.querySelector('#skills-wrapper-pie').addEventListener('mouseout', showSkillsIntro);
+	document.querySelector('#soft-list-pie').addEventListener('mouseout', showSkillsIntro);
+
+	function showContainer(containerId) {
+		// Hide all containers
+		document.querySelector('.skillsintro-container').style.display = 'none';
+		document.querySelector('.programming-container').style.display = 'none';
+		document.querySelector('.skills-wrapper').style.display = 'none';
+		document.querySelector('.soft-list').style.display = 'none';
+
+		// Show the selected container
+		document.querySelector(`.${containerId}`).style.display = 'block';
+		if (containerId=="skills-wrapper") {
+			document.querySelector(`.${containerId}`).style.display = 'flex';
+		}
+	}
+
+	function showSkillsIntro() {
+		// Hide all specific skill containers
+		document.querySelector('.programming-container').style.display = 'none';
+		document.querySelector('.skills-wrapper').style.display = 'none';
+		document.querySelector('.soft-list').style.display = 'none';
+	
+		// Show the skills intro container
+		document.querySelector('.skillsintro-container').style.display = 'block';
+	}
+});
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,117 @@
+// Function to dynamically set the focus class based on URL
+function setFocusBasedOnPathname() {
+    // Get the current pathname
+    var currentPathname = window.location.pathname;
+
+    // Define an object mapping pathnames to nav link IDs
+    var linkIdMap = {
+        "about.html": "about-link",
+        "skills.html": "skills-link",
+        "experience.html": "experience-link",
+        "projects.html": "projects-link",
+        "nutritional-intake-project.html":"projects-link",
+        "passenger-experience-project.html":"projects-link",
+        "book-recommendation-project.html":"projects-link"
+    };
+
+    // Determine the base pathname for comparison, if your site is in a subdirectory
+    var baseName = "/"; // Adjust this baseName as necessary
+
+    // Normalize the currentPathname to remove the baseName if your site is in a subdirectory
+    if (currentPathname.indexOf(baseName) === 0) {
+        currentPathname = currentPathname.substring(baseName.length);
+    }
+
+    // Find the corresponding nav link ID for the current pathname
+    var activeLinkId = linkIdMap[currentPathname];
+
+    // If there's a corresponding link ID, add the focus class to it
+    if (activeLinkId) {
+        var activeLink = document.getElementById(activeLinkId);
+        if (activeLink) activeLink.classList.add('focus');
+        var activeLinkTop = document.getElementById(activeLinkId + '-top');
+        if (activeLinkTop) activeLinkTop.classList.add('focus');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var headerContent = `
+    <div class="title">
+        <a href="index.html">Jiarui Tang</a>
+    </div>
+    <div>
+        <ul class="nav-links">
+        <li><a href="index.html#about" id="about-link-top">About</a></li>
+        <li><a href="index.html#skills" id="skills-link-top">Skills</a></li>
+        <li><a href="index.html#experience" id="experience-link-top">Experience</a></li>
+        <li><a href="index.html#projects" id="projects-link-top">Projects</a></li>
+        </ul>
+    </div>
+    `;
+    var scrollnavContent = `
+    <div class="title">
+        <a href="index.html">Jiarui Tang</a>
+    </div>
+    <div>
+        <ul class="nav-links">
+        <li><a href="index.html#about" id="about-link">About</a></li>
+        <li><a href="index.html#skills" id="skills-link">Skills</a></li>
+        <li><a href="index.html#experience" id="experience-link">Experience</a></li>
+        <li><a href="index.html#projects" id="projects-link">Projects</a></li>
+        </ul>
+    </div>
+    <!-- Button to close the overlay navigation -->
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>`;
+    var footerContent = `
+    <footer id="footer">
+		<div class="inner">
+			<ul class="icons">
+				<li><a href="https://www.linkedin.com/in/sherryjiaruitang" class="icon brands fa-linkedin" target=”_blank”><span class="label">LinkedIn</span></a></li>
+				<li><a href="https://github.com/jiaruitang" class="icon brands fa-github" target=”_blank”><span class="label">Github</span></a></li>
+				<li><a href="https://twitter.com/sherrytang14" class="icon brands fa-twitter" target=”_blank”><span class="label">Twitter</span></a></li>
+				<li><a href="mailto:jiarui1@email.unc.edu?subject=Reach%20Out%20from%20" class="icon solid fa-envelope" target=”_blank”><span class="label">Email</span></a></li>
+			</ul>
+			<ul class="copyright">
+				<li>&copy;  2024 Jiarui Tang</li>
+				<li>All Rights Reserved.</li>
+			</ul>
+		</div>
+    </footer>`;
+
+    // Example condition: if not the home page
+    if (window.location.pathname !== "/" && window.location.pathname !== "/index.html") {
+        document.getElementById('desktop-nav').innerHTML = headerContent;
+        document.getElementById('scroll-nav').innerHTML = scrollnavContent;
+        document.getElementById('pagefooter').innerHTML = footerContent;
+
+    }
+
+    // Call the function to set the focus based on pathname
+    setFocusBasedOnPathname();
+});
+
+// Highlight the currently viewed section in the sidebar.
+document.addEventListener("DOMContentLoaded", function() {
+    
+    function onScroll() {
+        let sidebarItems = document.querySelectorAll('#child-sidebar .sidebar-item');
+        if ($("#child-sidebar").css('display')=='none') {
+            sidebarItems = document.querySelectorAll('#mobile-sidebar .sidebar-item');
+        }
+        let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+        
+        sidebarItems.forEach((item) => {
+            let target = document.getElementById(item.getAttribute('data-target'));
+            console.log(target);
+            if (target.offsetTop - 300 <= scrollPosition && (target.offsetTop + target.offsetHeight + 100) > scrollPosition) {
+                sidebarItems.forEach((i) => i.classList.remove('active'));
+                item.classList.add('active');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', onScroll);
+    
+    onScroll(); // Initialize on load
+});
+
